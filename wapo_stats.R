2efb3e48f4ef0f55236e500711984a127dcc3be9
José Devezas <joseluisdevezas@gmail.com>
2018-07-09 14:50:58 +0100

    Improved performance. Should take 20 seconds per 10,000 documents.

--- wapo_stats.R
+++ wapo_stats.R
@@ -2,42 +2,59 @@ if (!require("pacman")) install.packages("pacman")
 
 pacman::p_load(
   "ggplot2",
-  "rjson"
+  "rjson",
+  "logging",
+  "plyr"
 )
 
-get_document_length <- function(filepath) {
-  df <- data.frame(doc_id=character(0), doc_len=integer(0), par_len=integer(0))
-  con = file(filepath, "r")
-  while (TRUE) {
-    line = readLines(con, n = 1)
+basicConfig()
 
-    if (length(line) == 0) {
+BATCH_SIZE <- 10000
+
+get_document_length <- function(filepath) {
+  res <- data.frame(doc_id=character(0), doc_len=integer(0), par_len=integer(0))
+  con = file(filepath, "r")
+  count <- 0
+
+  while (TRUE) {
+    lines = readLines(con, n = BATCH_SIZE)
+
+    if (length(lines) == 0) {
       break
     }
 
-    json <- fromJSON(line)
+    doc_lens <- lapply(lines, function(line) {
+      json <- fromJSON(line)
 
-    pars <- lapply(json$contents, function(c) {
-      if (length(c$subtype) > 0 && c$subtype == 'paragraph') {
-        gsub('<.*?>', '', c$content)
-      } else {
-        NULL
+      pars <- lapply(json$contents, function(c) {
+        if (length(c$subtype) > 0 && c$subtype == 'paragraph') {
+          gsub('<.*?>', '', c$content)
+        } else {
+          NULL
+        }
+      })
+
+      pars <- pars[!sapply(pars, is.null)]
+
+      doc_id = json$id
+      doc_len <- nchar(do.call(paste, pars))
+      par_len <- nchar(do.call(paste, head(pars, 3)))
+
+      if (length(doc_id) < 1 || length(doc_len) < 1 || length(par_len) < 1) {
+        return(data.frame(doc_id=character(0), doc_len=character(0), par_len=character(0)))
       }
-    })
-    
-    pars <- pars[!sapply(pars, is.null)]
 
-    doc_id = json$id
-    doc_len <- nchar(do.call(paste, pars))
-    par_len <- nchar(do.call(paste, head(pars, 3)))
-    
-    if (length(doc_id) < 1 || length(doc_len) < 1 || length(par_len) < 1) next
-    
-    df <- rbind(df, data.frame(doc_id=json$id, doc_len=doc_len, par_len=par_len))
+      data.frame(doc_id=json$id, doc_len=doc_len, par_len=par_len)
+    })
+
+    res <- rbind.fill(c(list(res), doc_lens))
+
+    count <- count + length(lines)
+    loginfo("%d documents processed", count)
   }
-  
+
   close(con)
-  df
+  res
 }
 
 doc_length <- get_document_length("~/Downloads/WashingtonPost.v2/data/TREC_Washington_Post_collection.v2.jl")
